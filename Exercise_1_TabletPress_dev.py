fb3158151169353e5b2b4e12dff43989d077e148
Christopher Zimmermann <christopher.zimmermann@gmx.de>
2022-03-19 12:04:55 +0100

    Implemented/updated script for exercise 1: read Tag Translation Table and automated information allocation (corresponding to datasets), automated plit of datasets based on sapparametername, more useful handling of dataframes

--- Exercise_1_TabletPress_dev.py
+++ Exercise_1_TabletPress_dev.py
@@ -21,10 +21,16 @@ import matplotlib.pyplot as plt
 # import dataset as pandas dataframe
 df = pd.read_csv('raw_data_tablet_press.csv', sep=',')
 # reduce dataset (every nth row) for development phase and store *.csv file 
-df = df.iloc[::5, :]
-df.to_csv('raw_data_tablet_press_short_5.csv')
+# df = df.iloc[::5, :]
+# df.to_csv('raw_data_tablet_press_short_5.csv')
 # use smaller dataset for development
 # df = pd.read_csv('raw_data_tablet_press_short.csv', sep=',')
+# import tags corresponding to dataset as pandas dataframe
+# note: *.csv file was extracted manually from Tag Translation Table - sheet
+df_TT = pd.read_csv('TT_Table_csv.csv', sep=',')
+
+# enter here, which dataset you want to visualize
+sensor_analysis = 'dpMeasuredValueStation1p001'
 
 # print some useful information about the dataset
 print(df.shape)
@@ -44,102 +50,72 @@ df = df.rename(columns={'Unnamed: 0': 'id', 'Unnamed: 10': 'time_stamp', 'time':
 df = df.sort_values(by='time_stamp', ascending=True)
 
 
-# split dataset for sensor-specific investigation
-df_dpMachineStatusS88StatusValue = df[df['sapparametername'].str.contains('dpMachineStatusS88StatusValue') == True]
-df_dpMachineStatusS88StatusName = df[df['sapparametername'].str.contains('dpMachineStatusS88StatusName') == True]
-# Tablets per hour * 1000:
-df_dpMeasuredValueStation1p001 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p001') == True]
+##############
+# Clean data #
+##############
+print(df.shape)
+# Omit datasets, in which values that are stated as "None"
+df = df[df['true_value'].str.contains('None') == False]
+# print(df['variant_type'].unique())
+# print(df.shape)
+# # Delete datasets, which have value type of NAN
+# df = df[df['variant_type'].str.contains('nan') == False] # similar result as with the "none" delete
+# Transform datatypes labeled with "double" and "int16" to float
+# create a list of all data types in data set
+# print(df['variant_type'].unique())
+list_variant_types = df['variant_type'].unique()
+df[df['variant_type'].str.contains('VariantType.Double') == True].astype({'true_value': float})
+df[df['variant_type'].str.contains('VariantType.Int16') == True].astype({'true_value': float})
+print(df.shape)
+# store existing data types for further investigation
+str_dtypes = df.dtypes
+# extract tagdescription and unit specification of datasets
+get_tagdescription = df_TT[df_TT['sapparametername'].str.contains(sensor_analysis) == True].tagdescription.values[0]
+get_param_unit = df_TT[df_TT['sapparametername'].str.contains(sensor_analysis) == True].tagdescription.values[0]
+get_param_unit = df_TT[df_TT['sapparametername'].str.contains(sensor_analysis) == True]
+get_param_unit = get_param_unit['[EN] Parameter Unit'].values[0]
 
-df_dpMeasuredValueStation1p002 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p002') == True]
-df_dpMeasuredValueStation1p003 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p003') == True]
-df_dpMeasuredValueStation1p004 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p004') == True]
-df_dpMeasuredValueStation1p005 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p005') == True]
-df_dpMeasuredValueStation1p006 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p006') == True]
+# create a dictionary with each specific dataset separated
+df_dict = {}
+# df_TT_dict = {}
+for state in df['sapparametername'].unique():
+    df_dict[state]=df[df['sapparametername']==state]
 
-df_dpMeasuredValueStation1p008 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p008') == True]
-
-df_dpMeasuredValueStation1p013 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p013') == True]
-
-df_dpMeasuredValueStation1p014 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p014') == True]
-df_dpMeasuredValueStation1p015 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p015') == True]
-
-
-# Füllkurve
-df_dpMeasuredValueStation1p039 = df[df['sapparametername'].str.contains('dpMeasuredValueStation1p039') == True]
-
-
-# omit faulty data "None" vs replacing
-# Whats the reason for "None"? Should it be ZERO or is it just an error?
-df_dpMeasuredValueStation1p001None = df_dpMeasuredValueStation1p001[df_dpMeasuredValueStation1p001['true_value'].str.contains('None') == True]
-df_dpMeasuredValueStation1p001NotNone = df_dpMeasuredValueStation1p001[df_dpMeasuredValueStation1p001['true_value'].str.contains('None') == False]
-df_dpMeasuredValueStation1p001 = df_dpMeasuredValueStation1p001NotNone
-
-df_dpMeasuredValueStation1p014NotNone = df_dpMeasuredValueStation1p014[df_dpMeasuredValueStation1p014['true_value'].str.contains('None') == False]
-df_dpMeasuredValueStation1p014 = df_dpMeasuredValueStation1p014NotNone
-
-df_dpMeasuredValueStation1p015NotNone = df_dpMeasuredValueStation1p015[df_dpMeasuredValueStation1p015['true_value'].str.contains('None') == False]
-df_dpMeasuredValueStation1p015 = df_dpMeasuredValueStation1p015NotNone
-
-df_dpMachineStatusS88StatusValueNotNone = df_dpMachineStatusS88StatusValue[df_dpMachineStatusS88StatusValue['true_value'].str.contains('None') == False]
-df_dpMachineStatusS88StatusValue = df_dpMachineStatusS88StatusValueNotNone
-
-df_dpMachineStatusS88StatusNameNotNone = df_dpMachineStatusS88StatusName[df_dpMachineStatusS88StatusName['true_value'].str.contains('None') == False]
-df_dpMachineStatusS88StatusName = df_dpMachineStatusS88StatusNameNotNone
-
-# replace 'None' by 0
-# print(df_dpMeasuredValueStation1p001[df_dpMeasuredValueStation1p001['true_value'].str.contains('None') == True])
-# print(df_dpMeasuredValueStation1p001[df_dpMeasuredValueStation1p001['true_value'].str.contains('None') == True].true_value.values)
-# # change 'None' to 0 Value?
-# mask = (df_dpMeasuredValueStation1p001['true_value'] == 'None')    
-# df_dpMeasuredValueStation1p001['true_value'] = df_dpMeasuredValueStation1p001['true_value'].mask(mask,0)
-# mask = (df_dpMachineStatusS88StatusValue['true_value'] == 'None')    
-# df_dpMachineStatusS88StatusValue['true_value'] = df_dpMachineStatusS88StatusValue['true_value'].mask(mask,0)
-# print(df_dpMeasuredValueStation1p001.true_value.values)
-
+# create list of df_dict
+list_df_dict = list(df_dict)
+# print(list_df_dict[0])
+# print(df_dict[list_df_dict[0]])
 
 ####################
 # Plotting section # 
 ####################
-# visualize datasets for first analysis
+x = df_dict[sensor_analysis].time_stamp.values
+y = df_dict[sensor_analysis].true_value.values.astype(float)
 
-# choose dataset for plotting
-x = df_dpMeasuredValueStation1p001.time_stamp.values
-x2 = df_dpMachineStatusS88StatusValue.time_stamp.values
-x3 = df_dpMeasuredValueStation1p014.time_stamp.values 
-#x = df_dpMeasuredValueStation1p001.id.values
-y = df_dpMeasuredValueStation1p001.true_value.values
-y2 = df_dpMachineStatusS88StatusValue.true_value.values
-y3 = df_dpMachineStatusS88StatusName.true_value.values
-# print(y2)
-# print(y3)
-y = df_dpMeasuredValueStation1p001.true_value.astype(float)
-y2 = df_dpMachineStatusS88StatusValue.true_value.astype(float)
-y3 = df_dpMeasuredValueStation1p014.true_value.astype(float)
-
+x2 = df_dict['dpMachineStatusS88StatusValue'].time_stamp.values
+y2 = df_dict['dpMachineStatusS88StatusValue'].true_value.values.astype(float)
 
 ############
 # Figure 1 #
 ############
-# print(df_dpMachineStatusS88StatusName.true_value)
-# fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(12,8), gridspec_kw={'height_ratios':[4,2,1]})
 plt.figure(1)
-#plt.bar(x=df_dpMeasuredValueStation1p001['time_stamp'], height=df_dpMeasuredValueStation1p001['true_value'], color='#087E8B')
-# plt.plot(x,y, lw=3, color='#087E8B')
-plt.bar(x, y, width=0.025, label='X', color='#087E8B')
-plt.title('Data Analysis', size=20)
+plt.plot(x,y, lw=3, color='#087E8B', label = (get_tagdescription))
+# plt.bar(x, y, width=0.025, label='X', color='#087E8B')
+plt.title(sensor_analysis, size=20)
 plt.xticks(rotation=45)
-plt.ylim(0,175)
-plt.ylabel('Value')
+# plt.ylim(0,105)
+plt.ylabel(get_tagdescription + ' [' + get_param_unit + ']')
+# plt.yticks((0,100))
 plt.legend()
 plt.grid(True)
 # ax1.set_xticklabels(x[::2], rotation = 45)
 
 par1y = plt.twinx()
-par1y.plot(x2, y2, 'k^', markersize = 15, label='S88')
+par1y.plot(x2, y2, 'k*', markersize = 10, label='S88')
 par1y.set_ylabel('Idle/Complete - Running - Held - Paused')
-par1y.legend()
+# par1y.legend()
 par1y.grid(True)
-
+# par1y.set_ylim(0,4)
 plt.show()
 
 
@@ -148,21 +124,39 @@ plt.show()
 ############
 plt.figure(2)
 #plt.bar(x=df_dpMeasuredValueStation1p001['time_stamp'], height=df_dpMeasuredValueStation1p001['true_value'], color='#087E8B')
-plt.plot(x,y, lw=3, color='#087E8B')
-# plt.bar(x, y, width=0.025, label='X', color='#087E8B')
-plt.title('Data Analysis', size=20)
+# plt.plot(x,y, lw=3, color='#087E8B')
+plt.bar(x, y, width=0.025, color='#087E8B', label = (get_tagdescription))
+plt.title(sensor_analysis, size=20)
 plt.xticks(rotation=45)
 # plt.ylim(0,175)
-plt.ylabel('Value')
+plt.ylabel(get_tagdescription + ' [' + get_param_unit + ']')
 plt.legend()
 plt.grid(True)
 # ax1.set_xticklabels(x[::2], rotation = 45)
 
 par1y = plt.twinx()
-par1y.plot(x2, y2, 'k^', markersize = 15, label='S88')
+par1y.plot(x2, y2, 'k*', markersize = 10, label='S88')
 par1y.set_ylabel('Idle/Complete - Running - Held - Paused')
-par1y.legend()
+# par1y.legend()
 par1y.grid(True)
-
 plt.show()
 
+############
+# Figure 3 #
+############
+plt.figure(3)
+plt.plot(x, y, '+', markersize = 10, color='#087E8B', label = (get_tagdescription))
+plt.title(sensor_analysis, size=20)
+plt.xticks(rotation=45)
+# plt.ylim(0,175)
+plt.ylabel(get_tagdescription + ' [' + get_param_unit + ']')
+plt.legend()
+plt.grid(True)
+# ax1.set_xticklabels(x[::2], rotation = 45)
+
+par1y = plt.twinx()
+par1y.plot(x2, y2, 'k*', markersize = 10, label='S88')
+par1y.set_ylabel('Idle/Complete - Running - Held - Paused')
+# par1y.legend()
+par1y.grid(True)
+plt.show()
