5b96acfa7141f1db3e7f23cb4c8dbd5f529df875
Christopher Zimmermann <christopher.zimmermann@gmx.de>
2022-03-21 02:06:16 +0100

    Updated script for exercise 2: Added some accuracy and cross-validation checks, added predictive models (KNNRegressor and KNNClassifier), code clean-up and further documentation follows

--- Exercise_2_PackagingLine_dev.py
+++ Exercise_2_PackagingLine_dev.py
@@ -3,12 +3,14 @@
 ####################################
 # 
 # Task:
-#   Feature importance and selection
-#   
+#   1.) Feature importance and selection
+#   2.) Predictive model development
+#   to do: code clean-up, some sections are redundant
+#   to do: code documentation
+#
 ###################
 # Import packages #
 ###################
-
 import pandas as pd
 import numpy as np
 import datetime
@@ -16,8 +18,12 @@ import matplotlib.pyplot as plt
 from IPython import get_ipython
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, KFold
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 # %matplotlib qt
 %matplotlib tk
 #get_ipython().run_line_magic('matplotlib', 'inline')
@@ -28,10 +34,8 @@ print(today)
 df = pd.read_csv('PackagingLineDataSet.csv', sep=',', encoding = 'latin1') 
 today = datetime.datetime.now()
 print(today)
-# for development phase: only a smaller chunk of the data
-df = df.iloc[::200, :]
-
-
+# for development phase: use only a smaller chunk of the data
+df = df.iloc[::20, :]
 
 ##############
 # Clean data #
@@ -44,7 +48,6 @@ df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_QualityTime_V = pd.t
 df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_OperatingTime_V = pd.to_timedelta(df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_OperatingTime_V).dt.total_seconds().astype(int)
 df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_UnplannedDownTime_V = pd.to_timedelta(df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_UnplannedDownTime_V).dt.total_seconds().astype(int)
 df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_LossTime_V = pd.to_timedelta(df.GRZ_L09_04_Cartoner_SX_V0803_01_Efficiency_VC_MZ_803_Z_R_LossTime_V).dt.total_seconds().astype(int)
-df.Date_Time = pd.to_datetime(df.Date_Time).astype(int) # maybe further scaling
 
 df = df.sort_values(by='Date_Time', ascending=True) # might not be necessary
 
@@ -117,25 +120,33 @@ plt.show()
 
 today = datetime.datetime.now()
 print(today)
+
+#################
+# Test Accuracy #
+#################
+score = model_LG_Prod.score(X_test_scaled, y_test)
+print("LG - first target: 'ProductionYN' - Test Accuracy Score", score)
+
+
 ########################################
 # Method 1 - second target: 'Category' #
 ########################################
 #
 # prepare datasets
-df_X2 = df.drop('Category', axis=1)
-df_X2 = df_X2.drop('ProductionYN', axis=1)
-df_X2 = df_X2.drop('Date_Time', axis=1)
-df_X2 = df_X2.drop('Generated Date', axis=1)
-df_X2 = df_X2.drop('ActivityName', axis=1)
-df_X2 = df_X2.drop('GRZ_L09_05_Seidenader_Recipe_Recipe__1__V', axis=1)
-df_X2 = df_X2.drop('GRZ_L09_02_Filler_APP_V0931_00_BatchProtocol_VC_MZ_931_EEZ_S_BatchName_V', axis=1)
-df_X2 = df_X2.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacControlLift_V', axis=1) # faulty data
-df_X2 = df_X2.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacMonitor_V', axis=1) # faulty data
+df_X1 = df.drop('Category', axis=1)
+df_X1 = df_X1.drop('ProductionYN', axis=1)
+df_X1 = df_X1.drop('Date_Time', axis=1)
+df_X1 = df_X1.drop('Generated Date', axis=1)
+df_X1 = df_X1.drop('ActivityName', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_05_Seidenader_Recipe_Recipe__1__V', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_02_Filler_APP_V0931_00_BatchProtocol_VC_MZ_931_EEZ_S_BatchName_V', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacControlLift_V', axis=1) # faulty data
+df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacMonitor_V', axis=1) # faulty data
 
 # define second target
-df_y2 = df['Category']
+df_y1 = df['Category']
 # split dataset
-X_train, X_test, y_train, y_test = train_test_split(df_X2, df_y2, test_size=0.25, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(df_X1, df_y1, test_size=0.25, random_state=4)
 
 ss = StandardScaler()
 X_train_scaled = ss.fit_transform(X_train)
@@ -163,6 +174,12 @@ plt.show()
 
 today = datetime.datetime.now()
 print(today)
+
+#################
+# Test Accuracy #
+#################
+score = model_LG_Cat.score(X_test_scaled, y_test)
+print("LG - second target: 'Category' - Test Accuracy Score", score)
 
 ###########################################
 # Method 2 - first target: 'ProductionYN' #
@@ -213,6 +230,15 @@ plt.tick_params(
     labelbottom=False) # labels along the bottom edge are off
 plt.show()
 
+####################
+# Cross validation #
+####################
+
+scores = cross_val_score(model_XGB_Prod, X_train, y_train, cv=2)
+print("XGB - first target: 'ProductionYN' - Mean cross-validation score: %.2f" % scores.mean())
+kfold = KFold(n_splits=2, shuffle=True)
+kf_cv_scores = cross_val_score(model_XGB_Prod, X_train, y_train, cv=kfold )
+print("XGB - first target: 'ProductionYN' - K-fold CV average score: %.2f" % kf_cv_scores.mean())
 
 ########################################
 # Method 2 - second target: 'Category' #
@@ -263,4 +289,115 @@ plt.tick_params(
 plt.show()
 today = datetime.datetime.now()
 print(today)
+
+####################
+# Cross validation #
+####################
+
+scores = cross_val_score(model_XGB_Cat, X_train, y_train, cv=2)
+print("XGB - second target: 'Category' - Mean cross-validation score: %.2f" % scores.mean())
+kfold = KFold(n_splits=2, shuffle=True)
+kf_cv_scores = cross_val_score(model_XGB_Cat, X_train, y_train, cv=kfold )
+print("XGB - second target: 'Category' - K-fold CV average score: %.2f" % kf_cv_scores.mean())
+
+
+####################
+# Predictive Model #
+####################
+# KNeighborsClassifier
+# tbd....
+
+# prepare the datasets
+df_X1 = df.drop('ProductionYN', axis=1)
+df_X1 = df_X1.drop('Date_Time', axis=1)
+df_X1 = df_X1.drop('Category', axis=1)
+df_X1 = df_X1.drop('Generated Date', axis=1)
+df_X1 = df_X1.drop('ActivityName', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_05_Seidenader_Recipe_Recipe__1__V', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_02_Filler_APP_V0931_00_BatchProtocol_VC_MZ_931_EEZ_S_BatchName_V', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacControlLift_V', axis=1) # faulty dataset
+df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacMonitor_V', axis=1) # faulty dataset
+#################################
+# now drop unimportant features #
+#################################
+# 
+unimportant_features = importances_XGB_Cat[abs(importances_XGB_Cat['Importance'].values) <= 0.025*max(abs(importances_XGB_Cat['Importance'].values))]
+df_X1 = df_X1.drop(unimportant_features.Attribute, axis=1)
+
+
+# define first target
+df_y1 = df['Category']
+# df_y1 = df['GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_IX_456_B5VacLow_V']
+# df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_IX_456_B5VacLow_V', axis=1)
+
+#Split the data into test and train
+X_train, X_test, y_train, y_test = train_test_split(df_X1, df_y1, test_size=0.25,random_state=4)
+
+# scale
+ss = StandardScaler()
+X_train_scaled = ss.fit_transform(X_train)
+X_test_scaled = ss.transform(X_test)
+
+model_KNN=KNeighborsClassifier(n_neighbors=7, metric='euclidean')
+model_KNN.fit(X_train_scaled,y_train)
+
+y_pred=model_KNN.predict(X_test_scaled)
+
+#################
+# Test Accuracy #
+#################
+score = model_KNN.score(X_test_scaled, y_test)
+print("KNN - Test Accuracy Score", score)
+
+# Model Accuracy, how often is the classifier correct?
+print("KNN - Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+scores = cross_val_score(model_KNN, X_train_scaled, y_train, cv=2)
+print("KNN - Mean cross-validation score: %.2f" % scores.mean())
+kfold = KFold(n_splits=2, shuffle=True)
+kf_cv_scores = cross_val_score(model_KNN, X_train_scaled, y_train, cv=kfold )
+print("KNN - K-fold CV average score: %.2f" % kf_cv_scores.mean())
+
+
+####################
+# Predictive Model #
+####################
+# KNeighborsRegressor
+
+# prepare the datasets
+df_X1 = df.drop('ProductionYN', axis=1)
+df_X1 = df_X1.drop('Date_Time', axis=1)
+df_X1 = df_X1.drop('Category', axis=1)
+df_X1 = df_X1.drop('Generated Date', axis=1)
+df_X1 = df_X1.drop('ActivityName', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_05_Seidenader_Recipe_Recipe__1__V', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_02_Filler_APP_V0931_00_BatchProtocol_VC_MZ_931_EEZ_S_BatchName_V', axis=1)
+df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacControlLift_V', axis=1) # faulty dataset
+df_X1 = df_X1.drop('GRZ_L09_04_Cartoner_SX_V0456_01_RobotTool_VC_MZ_456_A_VacMonitor_V', axis=1) # faulty dataset
+
+
+# define first target
+# df_y1 = df['Category']
+df_y1 = df['GRZ_L09_01_Blistering_PLC_Modules__Default__Global_PV_tPeCupEdSW_fLatchDiffMm_V']
+df_X1 = df_X1.drop('GRZ_L09_01_Blistering_PLC_Modules__Default__Global_PV_tPeCupEdSW_fLatchDiffMm_V', axis=1)
+
+#Split the data into test and train
+X_train, X_test, y_train, y_test = train_test_split(df_X1, df_y1, test_size=0.25,random_state=4)
+
+# scale
+ss = StandardScaler()
+X_train_scaled = ss.fit_transform(X_train)
+X_test_scaled = ss.transform(X_test)
+
+model_KNN_cat=KNeighborsRegressor(n_neighbors=7, metric='euclidean')
+model_KNN_cat.fit(X_train_scaled,y_train)
+
+y_pred=model_KNN_cat.predict(X_test_scaled)
+
+#################
+# Test Accuracy #
+#################
+score = model_KNN_cat.score(X_test_scaled, y_test)
+print("KNNRegressor - Test Accuracy Score", score)
+
 
